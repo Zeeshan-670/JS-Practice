6cc1f49f9749a1faf067e8ad3f62aef1713461f1
Muhammad Zeeshan Muneer <muneerzeeshan670@gmail.com>
2023-02-21 07:52:27 -0800

    Starts section no 12

--- Section-11/script.js
+++ Section-11/script.js
@@ -572,11 +572,11 @@ btnSort.addEventListener('click', e => {
  * * 1.
  */
 
-const bankDepositSum = accounts
-  .flatMap(acc => acc.movements)
-  .filter(mov => mov > 0)
-  .reduce((sum, cur) => sum + cur, 0);
-console.log(bankDepositSum);
+// const bankDepositSum = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov > 0)
+//   .reduce((sum, cur) => sum + cur, 0);
+// console.log(bankDepositSum);
 
 /**
  * * 2.
@@ -586,8 +586,106 @@ console.log(bankDepositSum);
 //   .flatMap(acc => acc.movements)
 //   .filter(dep => dep >= 1000).length;
 
-const numDeposits1000 = accounts
-  .flatMap(acc => acc.movements)
-  .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
 
-console.log(numDeposits1000);
+// console.log(numDeposits1000);
+
+/**
+ * * 3
+ */
+
+// const { deposits, withdarwals } = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       cur > 0 ? (sums.deposits += cur) : (sums.withdarwals += cur);
+//       return sums;
+//     },
+//     { deposits: 0, withdarwals: 0 }
+//   );
+
+// console.log(deposits);
+// console.log(withdarwals);
+
+/**
+ * * 4
+ */
+
+// this is a nice title -> This Is a Nice Title
+// const convertTitleCase = title => {
+//   const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+//   const expections = ['a', 'an', 'the', 'And', 'but', 'or', 'on', 'in', 'with'];
+//   const titleCase = title
+//     .toLowerCase()
+//     .split(' ')
+//     .map(word => (expections.includes(word) ? word : capitalize(word)))
+//     .join(' ');
+
+//   console.log(capitalize(titleCase));
+// };
+
+// convertTitleCase('this is a nice title');
+// convertTitleCase('this is a LONG title but not to long');
+// convertTitleCase('and here is another title with an EXAMPLE');
+
+// TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+console.log(dogs);
+
+// 2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  }`
+);
+
+// 3
+
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+// 4.
+
+("Matilda and Alice and Bob's dogs eat too much!");
+("Sarah and John and Michael's dogs eat too little!");
+
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+// 6.
+
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+console.log(dogs.some(checkEatingOkay));
+
+// 7.
+console.log(dogs.filter(checkEatingOkay));
+
+// 8.
+
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
