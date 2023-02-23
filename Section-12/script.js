676c5b08cbe5077e90d37112a418dc18e5812113
Muhammad Zeeshan Muneer <muneerzeeshan670@gmail.com>
2023-02-22 18:38:10 -0800

    working with dates

--- Section-12/script.js
+++ Section-12/script.js
@@ -16,14 +16,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-02-18T21:31:17.178Z',
+    '2023-02-20T07:42:02.383Z',
+    '2023-02-21T09:15:04.904Z',
+    '2023-02-11T10:17:24.185Z',
+    '2023-02-14T14:11:59.604Z',
+    '2023-02-17T17:01:17.194Z',
+    '2023-02-20T23:36:17.929Z',
+    '2023-02-09T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -36,14 +36,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2023-02-18T13:15:33.035Z',
+    '2023-02-20T09:48:16.867Z',
+    '2023-02-21T06:04:23.907Z',
+    '2023-02-11T14:18:46.235Z',
+    '2023-02-14T16:33:06.386Z',
+    '2023-02-17T14:43:26.374Z',
+    '2023-02-20T18:49:59.371Z',
+    '2023-02-09T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,6 +81,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = date => {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1 / (1000 * 60 * 60 * 24)));
+
+  const dayPassed = calcDaysPassed(new Date(), date);
+
+  if (dayPassed === 0) return 'Today';
+  if (dayPassed === 1) return 'Yesterday';
+  if (dayPassed <= 7) return `${dayPassed} days`;
+  console.log(dayPassed);
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -91,11 +107,7 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const date = new Date([acc.movementsDates[i]]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
-
+    const displayDate = formatMovementDate(date);
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -166,9 +178,9 @@ let currentAccount;
 /**
  * * Fake Login
  */
-// currentAccount = account1;
-// updateUI(currentAccount);
-// containerApp.style.opacity = 100;
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -461,12 +473,12 @@ console.log(+(2.345).toFixed(2));
 // future.setFullYear(2040);
 // console.log(future);
 
-const future = new Date(2037, 10, 19, 15, 23);
-console.log(+future);
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(+future);
 
-const calcDaysPassed = (date1, date2) =>
-  Math.abs(date2 - date1 / (1000 * 60 * 60 * 24));
+// // const calcDaysPassed = (date1, date2) =>
+// //   Math.abs(date2 - date1 / (1000 * 60 * 60 * 24));
 
-const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+// const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
 
-console.log(days1);
+// console.log(days1);
