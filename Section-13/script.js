792d6724548227d7c5f15f3166afc0d84eaa421a
M.Zeeshan Muneer <muneerzeeshan670@gmail.com>
2023-02-24 16:58:34 +0500

    Starting Section no 13

--- Section-13/script.js
+++ Section-13/script.js
@@ -31,5 +31,111 @@ document.addEventListener('keydown', function (e) {
 });
 
 /**
- *
+ * * Lecture No 5 of Section 13
+ * * Selecting, Creating, And Deleting  Elements
  */
+
+/**
+ * * 1. Selecting Elements
+ */
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+/**
+ * * 2. Creating & Inserting Elements
+ * * insertAdjacentHTML
+ */
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookied for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+
+header.before(message);
+// header.after(message);
+
+/**
+ * * 3. Delete Elements
+ */
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  // message.remove();
+  message.parentElement.removeChild(message);
+});
+
+/**
+ * * Lecture No 6 of Section 13
+ * * Styles, Attributes & Classes
+ */
+
+/**
+ * * 1. Styles
+ */
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '100%';
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+/**
+ * * 2. Attributes
+ */
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non Standard
+console.log(logo.getAttribute('alt'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes
+
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c', 'j');
+logo.classList.contains('c', 'j');
+
+// Don't use
+logo.className = 'jonas';
